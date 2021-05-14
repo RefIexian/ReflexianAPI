b37878c7306544d963f5c3dcf9eae5f6c3c27ccf
Raymond <raymondpattend@icloud.com>
2021-05-14 13:59:14 -0400

    Bug Fix

--- src/main/java/com/reflexian/publicapi/exceptions/APIThrottleException.java
+++
@@ -1,7 +0,0 @@
-package com.reflexian.publicapi.exceptions;
-
-public class APIThrottleException extends ReflexianException {
-    public APIThrottleException() {
-        super("You have reached the API rate limit!");
-    }
-}
--- src/main/java/com/reflexian/publicapi/exceptions/KeyBlacklistedException.java
+++
@@ -1,9 +0,0 @@
-package com.reflexian.publicapi.exceptions;
-
-public class KeyBlacklistedException extends ReflexianException{
-
-    public KeyBlacklistedException() {
-        super("Your API key is blacklisted!");
-    }
-
-}
